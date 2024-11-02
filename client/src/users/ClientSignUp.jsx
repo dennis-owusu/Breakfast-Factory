257a0881e4f8f0ebec01af462727b5a296a3b500
dennis-owusu <kwesimodestygh111@gmail.com>
2024-11-02 14:38:47 -0800

    problem solved

--- client/src/users/ClientSignUp.jsx
+++ client/src/users/ClientSignUp.jsx
@@ -9,7 +9,7 @@ const ClientSignUp = () => {
     const [loading, setLoading] = useState(false)
 
     const navigate = useNavigate()
-    const [formData, setFormData] = useState({ name: '', phoneNumber: '', email: ''});
+    const [formData, setFormData] = useState({});
   
     const handleChange = (e) => {
       setFormData({...formData, [e.target.id]:e.target.value.trim()})
@@ -67,9 +67,9 @@ const ClientSignUp = () => {
        <h4 style={{fontFamily:'Poppins', fontWeight:500, fontSize:'24px', lineHeight:'36px'}}>Create an account</h4>
        <form onSubmit={handleSubmit} className='flex w-[90%] flex-col justify-center gap-2 -space-y-5 mt-8 items-center mx-auto py-20'>
         <div className='flex flex-col gap-4 w-full'>
-        <input type="text" id="name" value={formData.name} onChange={handleChange} className='py-3 outline-none px-3 active:border-[#FA9302] w-full mt-3 rounded-3xl border-2 justify-start flex border-[#FA9302]' placeholder='Your Name'/>
-        <input type="text" id="email" value={formData.email} onChange={handleChange} className='py-3 outline-none px-3 active:border-[#FA9302] w-full mt-3 rounded-3xl border-2 justify-start flex border-[#FA9302]' placeholder='Your Email'/>
-        <input type="text" value={formData.phoneNumber} id="phoneNumber" onChange={handleChange} className='py-3 outline-none px-3 active:border-[#FA9302] w-full rounded-3xl border-2 border-[#FA9302]' placeholder='Phone Number'/>
+        <input type="text" id="name" onChange={handleChange} className='py-3 outline-none px-3 active:border-[#FA9302] w-full mt-3 rounded-3xl border-2 justify-start flex border-[#FA9302]' placeholder='Your Name'/>
+        <input type="text" id="email" onChange={handleChange} className='py-3 outline-none px-3 active:border-[#FA9302] w-full mt-3 rounded-3xl border-2 justify-start flex border-[#FA9302]' placeholder='Your Email'/>
+        <input type="text" id="phoneNumber" onChange={handleChange} className='py-3 outline-none px-3 active:border-[#FA9302] w-full rounded-3xl border-2 border-[#FA9302]' placeholder='Phone Number'/>
         </div>
         <div className='w-full'>
           <button type="submit" className='bg-[#FA9302] text-white mt-6 mb-5 rounded-3xl py-3 w-full text-center mx-auto'>Continue</button>
