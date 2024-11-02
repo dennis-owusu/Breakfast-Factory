2859838c53012a5bf6bd7cac628f31bf8b1d166b
dennis-owusu <kwesimodestygh111@gmail.com>
2024-11-02 14:55:09 -0800

    token removed from client route file

--- api/routes/clientUser.route.js
+++ api/routes/clientUser.route.js
@@ -1,11 +1,10 @@
 import express from 'express';
 import { allClients, createClient, loginClient } from '../controllers/clientUser.controller.js';
-import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.post('/create/client-user', createClient)
-router.post('/login/client-user', verifyToken, loginClient)
+router.post('/login/client-user', loginClient)
 router.get('/create/client-user', allClients)
 
  
