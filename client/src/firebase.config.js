ea7f8aca85a58361615839ada096b018df637fdd
Dennis <kwesimodestygh111@gmail.com>
2024-11-05 13:11:18 -0800

    firebase auth completed

--- client/src/firebase.config.js
+++ client/src/firebase.config.js
@@ -9,7 +9,8 @@ const firebaseConfig = {
   projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_FIREBASE_APPID
+  appId: import.meta.env.VITE_FIREBASE_APPID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENTID
 };
 
 // Initialize Firebase
